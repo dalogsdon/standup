931f4fe59f8a3595003849cae28a11a265ff35b4
Drew Logsdon <dalogsdon@gmail.com>
2019-05-29 22:30:00 -0500

    Fix favicon path

--- components/Meta.js
+++ components/Meta.js
@@ -4,7 +4,7 @@ const Meta = () => (
     <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
-        <link rel="shortcut icon" href="/static/favicon.png" />
+        <link rel="shortcut icon" href="../static/favicon.png" />
         <link href="https://fonts.googleapis.com/css?family=Bungee&display=swap" rel="stylesheet"></link>
         <title>Standup</title>
     </Head>
